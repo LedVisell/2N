25ffcba6f4f0202ba0f48587c05dfbb0e57cdea3
LedVisell <108343416+LedVisell@users.noreply.github.com>
2022-07-12 22:32:46 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите  координаты чисел: ");
+
+void  Distance(float X1, float X2, float Y1, float Y2,
+                float Z1, float Z2);
+
+double Distance = Convert.ToDouble32(Console.ReadLine());
+{
+    double d = Math.Sqrt(Math.Pow(X2 - X1, 2) +
+                         Math.Pow(Y2 - Y1, 2) +
+                         Math.Pow(Z2 - Z1, 2));
+                                   
+    Console.WriteLine("Distance " + d);
+    
+}
